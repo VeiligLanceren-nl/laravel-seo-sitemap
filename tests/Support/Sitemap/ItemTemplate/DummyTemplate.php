4e2c1ac5ac60ddfde898b3cf7b82c5044bf7c2a5
Niels Hamelink <niels@gyvex.com>
2025-05-07 10:00:38 +0200

    Added sitemapUsing macro

---
+++ tests/Support/Sitemap/ItemTemplate/DummyTemplate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Support\Sitemap\ItemTemplate;
+
+use Traversable;
+use Illuminate\Routing\Route as LaravelRoute;
+use VeiligLanceren\LaravelSeoSitemap\Sitemap\Item\Url;
+use VeiligLanceren\LaravelSeoSitemap\Sitemap\SitemapItemTemplate;
+
+class DummyTemplate implements SitemapItemTemplate
+{
+    /**
+     * @param LaravelRoute $route
+     * @return iterable<Url>
+     */
+    public function generate(LaravelRoute $route): iterable
+    {
+        return [
+            Url::make('https://example.com/first'),
+            Url::make('https://example.com/second'),
+        ];
+    }
+
+    public function getIterator(): Traversable
+    {
+        yield from $this->generate(app(LaravelRoute::class));
+    }
+}
